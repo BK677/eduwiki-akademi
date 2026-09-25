0a7c868c9a1c37e4e5e465399dfab8699bd23f38
BK677 <burakkaraizm@gmail.com>
2026-09-25 20:27:41 +0300

    Set base path for Vite configuration
    Add base path configuration for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig(() => {
   return {
+    base: '/eduwiki-akademi/', // <-- BU SATIRI TAM BURAYA EKLEYİN
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
